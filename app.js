9144ed1fa38a890b234e3e9dc9628e81fde6789a
shwetapurushe <shwetapurushe@gmail.com>
2015-08-05 17:09:20 -0400

    angular app up and running

---
+++ app.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Shweta on 8/5/15.
+ */
+
+(function(){
+
+    angular.module('crumbs', []);
+
+    angular.module('crumbs').controller('mainController', crumbController);
+
+    crumbController.$inject = [];
+    function crumbController (){
+
+    }
+})();
+
