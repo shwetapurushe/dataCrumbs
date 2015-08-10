6db858ad83057b4cd9d7efa1a2de39e50b4d2bdb
shwetapurushe <shwetapurushe@gmail.com>
2015-08-10 15:57:58 -0400

    WeaveService for managing Weave Tree Node API

---
+++ src/WeaveService.js
@@ -0,0 +1,42 @@
+/**
+ * Created by Shweta on 8/10/15.
+ * this file helps in managing data retrieval from Weave
+ * @ author shweta purushe
+ */
+
+(function(){
+    angular.module('crumbs').service('WeaveService', weaveService);
+
+    function weaveService (){
+        var that = this;
+
+        that.weave;
+        that.weave_Tree;
+
+        //static function
+        /** requests the WeaveNodeTree hierarchy comprised of IWeaveTreeNode objects**/
+        that.request_WeaveTree = function (){
+            if(that.check_WeaveReady())
+            {
+                if(!that.weave_Tree){
+                    that.weave_Tree = new that.weave.WeaveTreeNode();
+                    return that.weave_Tree;
+                }
+                else
+                    return that.weave_Tree;
+            }
+            else{
+                setTimeout(that.request_WeaveTree, 100);
+            }
+        };
+
+        //static function
+        that.check_WeaveReady = function(){
+
+            if(!that.weave)
+                that.weave = document.getElementById('weave');
+            return that.weave && that.weave.WeavePath && that.weave._jsonCall;
+        };
+
+    }
+})();
